40dcac4a529e22e6eb72f83b40cfb8947549d9d5
sg4828990-cmd <sg4828990@gmail.com>
2026-01-28 18:43:06 +0530

    Update web.py

--- web.py
+++ web.py
@@ -4,7 +4,7 @@ web_app = Flask(__name__)
 
 @web_app.route('/')
 def home():
-    return "Bot is running by SPIDY UNIVERSE"
+    return "Bot is running by LEGEND TEAM"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8081)))
